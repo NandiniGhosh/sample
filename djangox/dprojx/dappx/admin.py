b1f280dca47a34dab1fcabcfa3205e3f456b0456
NandiniGhosh <nandinighosh3008@gmail.com>
2019-02-20 03:05:23 -0500

    first commit

---
+++ djangox/dprojx/dappx/admin.py
@@ -0,0 +1,7 @@
+
+# Register your models here.
+from django.contrib import admin
+from dappx.models import UserProfileInfo, User
+# Register your models here.
+
+admin.site.register(UserProfileInfo)
